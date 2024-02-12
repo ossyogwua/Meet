e71df2e33397a993c5077e48e67ad7907c1a5e4d
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2024-02-12 15:48:25 +0100

    debuggin

--- src/api.js
+++ src/api.js
@@ -60,7 +60,6 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
   const token = await getAccessToken();
