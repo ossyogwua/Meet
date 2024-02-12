50bcbdab074ce3e4be1e69deb57e571fcebf64e7
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2024-02-12 15:47:12 +0100

    debugging

--- src/api.js
+++ src/api.js
@@ -58,19 +58,19 @@ export const getEvents = async () => {
     return mockData;
   }
 
-  if ( !navigator.onLine ) {
+  if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
     NProgress.done();
     return events ? JSON.parse(events) : [];
   }
   const token = await getAccessToken();
 
-
   if (token) {
     removeQuery();
     const url =
       "https://e6892sc8qf.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
       "/" +
+      token;
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
